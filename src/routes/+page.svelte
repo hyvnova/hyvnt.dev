9ad526fdcc037cb3b34a12a93bb3c58bd3e7bd6b
Hyvnt <hendrickrodriguez.py@gmail.com>
2025-04-19 19:28:04 -0400

    Original. Art.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<main
+    class="grid place-content-center 
+    h-screen min-h-screen
+    "
+>
+
+    <h1 class='italic'>
+        <span class="not-italic font-normal text-purple-800 border-white border-b">H</span>yvn't
+    </h1>
+    <p class="font-thin italic">What are you looking for?</p>
+</main>
